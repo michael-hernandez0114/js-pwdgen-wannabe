409d30cd6dd94c3d2fe11f1047e79714f94a9e7a
Michael Hernandez <gammabeta010@gmail.com>
2020-02-17 15:23:50 +0100

    Funziona con tre prompt

--- js/mainscript.js
+++ js/mainscript.js
@@ -1,1 +1,10 @@
-alert('testing')
+var nome;
+var cognome;
+var colore;
+const venti = 20;
+
+nome = prompt('Please enter your name');
+cognome = prompt('Please enter your last name');
+colore = prompt('Please enter your favorite color');
+
+document.getElementById('pwd').innerHTML = nome + cognome + colore + venti;
